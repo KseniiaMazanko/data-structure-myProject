050fc9f85a62c965351614e5b4a07dc53c752098
KseniiaMazanko <ksmazanko@gmail.com>
2022-02-21 19:56:16 -0500

    2 modules added

---
+++ src/main/java/com/example/datastructuremyproject/DataStructureMyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastructuremyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureMyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureMyProjectApplication.class, args);
+    }
+
+}
